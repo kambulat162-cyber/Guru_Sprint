5cd2307ab417ca095cbc445bd0c77fed35bfb931
bezumez <kambulat162@gmail.com>
2026-03-25 09:12:12 +0300

    GS-1-2

---
+++ src/lesson1_task2.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val applePrice = 15.00
+    val apllesAmout = 5
+    var totalPrice = applePrice * apllesAmout
+    totalPrice = totalPrice +(totalPrice * 0.5)
+    println(totalPrice)
+
+
+}
