68b0d525e4fd5552cee10a4316b8f4f819ccc88e
bezumez <kambulat162@gmail.com>
2026-03-25 09:47:29 +0300

    GS-1-3 task compleatedd

--- src/lesson1/lesson1_task3.kt
+++ src/lesson1/lesson1_task3.kt
@@ -2,6 +2,17 @@ package lesson1
 
 // сделали задачу
 
+fun main(){
+    val botName = "botic"
+    val version = 1.2
+    println("Привет! Я $botName моя версия - $version")
+    println{"${2 + 2}"}
+
+
+
+
+}
+
 
 
 
