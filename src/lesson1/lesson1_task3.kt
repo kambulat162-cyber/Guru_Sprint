c549932e14310b206f993c4c24616cd4175c9941
bezumez <kambulat162@gmail.com>
2026-03-25 09:50:22 +0300

    GS-1-3 I've removed the extra lines

--- src/lesson1/lesson1_task3.kt
+++ src/lesson1/lesson1_task3.kt
@@ -1,47 +1,8 @@
 package lesson1
-
 // сделали задачу
-
 fun main(){
     val botName = "botic"
     val version = 1.2
     println("Привет! Я $botName моя версия - $version")
     println{"${2 + 2}"}
-
-
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
